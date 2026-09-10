6422339b0296e1d2766e2185f186de53f72c2431
Jonathan Rehem <jonathan.rehem@outlook.com>
2026-09-10 09:45:06 -0300

    fix: align todo list mutation contracts

--- modules/lifeforge--todo-list/server/routes/priorities.ts
+++ modules/lifeforge--todo-list/server/routes/priorities.ts
@@ -3,6 +3,12 @@ import z from 'zod'
 import forge from '../forge'
 import todoListSchemas from '../schema'
 
+const priorityInput = todoListSchemas.priorities.omit({
+  id: true,
+  collectionId: true,
+  collectionName: true
+})
+
 export const list = forge
   .query({
     description: 'Get all todo priorities',
@@ -20,7 +26,7 @@ export const create = forge
   .mutation({
     description: 'Create a new priority level',
     input: {
-      body: todoListSchemas.priorities
+      body: priorityInput
     },
     output: {
       CREATED: todoListSchemas.priorities
@@ -39,7 +45,7 @@ export const update = forge
       query: z.object({
         id: z.string()
       }),
-      body: todoListSchemas.priorities
+      body: priorityInput
     },
     existenceCheck: {
       query: { id: 'priorities' }
