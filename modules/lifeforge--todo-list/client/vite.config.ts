b616b667be2a08b6e76af1d731e567e0d1c936ad
Jonathan Rehem <jonathan.rehem@outlook.com>
2026-09-16 17:58:57 -0300

    drill: apply CI fixes

--- modules/lifeforge--todo-list/client/vite.config.ts
+++ modules/lifeforge--todo-list/client/vite.config.ts
@@ -1,5 +1,7 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineModuleClientConfig } from '@lifeforge/configs/vite'
 
-export default defineModuleClientConfig({
-  dirname: __dirname
-})
+export default defineModuleClientConfig(
+  { dirname: __dirname },
+  { plugins: [tailwindcss()] }
+)
